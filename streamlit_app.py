b7c054760fda02fe6749006e6c7a78b8cd6481e1
Distracted Penguin <distractedpen@users.noreply.github.com>
2022-08-25 15:34:31 -0400

    Update streamlit_app.py
    typo

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,7 +16,7 @@ my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/da
 # pick list for user to choose fruits to include
 my_fruit_list = my_fruit_list.set_index('Fruit')
 
-fruits_select = st.multiselect("Pick some fruits: ", list(my_fruit_list.index), default=['Avocado', 'Strawberries'])
+fruits_selected = st.multiselect("Pick some fruits: ", list(my_fruit_list.index), default=['Avocado', 'Strawberries'])
 # display the full table on page
 fruits_to_show = my_fruit_list.loc[fruits_selected]
 st.dataframe(fruits_to_show)
