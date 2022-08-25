3c2fa9d9af4640d117f35ec95eccbc0e9f6ac4f7
Distracted Penguin <distractedpen@users.noreply.github.com>
2022-08-25 15:30:30 -0400

    Update streamlit_app.py
    set default on multiselect

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,6 +16,6 @@ my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/da
 # pick list for user to choose fruits to include
 my_fruit_list = my_fruit_list.set_index('Fruit')
 
-streamlit.multiselect("Pick some fruits: ", list(my_fruit_list.index))
+streamlit.multiselect("Pick some fruits: ", list(my_fruit_list.index), ['Avacado', 'Strawberries'])
 # display the full table on page
 streamlit.dataframe(my_fruit_list)
