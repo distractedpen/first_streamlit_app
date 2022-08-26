8211aee5db99442cbbd0a5e123dace7aba882a16
Distracted Penguin <distractedpen@users.noreply.github.com>
2022-08-26 12:53:29 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,7 @@ import streamlit as st
 import pandas as pd
 import requests
 import snowflake.connector
+import urllib.error import URLError
 
 
 st.title('My Parents New Healthy Diner')
@@ -24,16 +25,18 @@ fruits_selected = st.multiselect("Pick some fruits: ", list(my_fruit_list.index)
 fruits_to_show = my_fruit_list.loc[fruits_selected]
 st.dataframe(fruits_to_show)
 
-st.header('Fruityvice Fruit Advice!')
-fruit_choice = st.text_input('What fruit would you like information about?', "Kiwi")
-st.write('The user entered', fruit_choice)
-# get the data from fruityvice
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-# convert from json to pd dataframe
-fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
-# display pd dataframe
-st.dataframe(fruityvice_normalized)
 
+st.header('Fruityvice Fruit Advice!')
+try:
+  fruit_choice = st.text_input('What fruit would you like information about?')
+  if not fruit_choice:
+    st.error("Please select a fruit to get information.")
+  else:
+    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+    fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
+    st.dataframe(fruityvice_normalized)
+except URLError as e:
+  st.error()
 
 conn = snowflake.connector.connect(**st.secrets["snowflake"])
 cur = conn.cursor()
@@ -44,3 +47,5 @@ st.dataframe(data_rows)
 
 new_fruit = st.text_input("What fruit would you like to add?")
 st.text("Thanks for adding " + new_fruit)
+if new_fruit:
+  cur.execute("insert into fruit_load_list values ?", new_fruit)
